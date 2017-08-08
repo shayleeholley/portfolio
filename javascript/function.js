d2d3de06121075dbde7680433a39c20b68b9c86d
Shaylee Holley <Shaylee Holley>
2017-08-07 20:42:26 -0600

    pulled

--- javascript/function.js
+++ javascript/function.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 1000, function(){
-
+ 
         window.location.hash = hash;
       });
     } 
